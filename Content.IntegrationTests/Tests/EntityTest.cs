964d43f17337f5d4add8713082263c30d5916292
akino-tech <pimnikcoldun@gmail.com>
2026-09-19 16:44:33 +0300

    Revert "Stabilize entity lifecycle test against gas tank explosions"
    This reverts commit e2219160c09dde3ec38809e57f8cb423c592571b.

--- Content.IntegrationTests/Tests/EntityTest.cs
+++ Content.IntegrationTests/Tests/EntityTest.cs
@@ -4,7 +4,6 @@ using System.Numerics;
 using System.Text;
 using Content.IntegrationTests.Fixtures;
 using Content.IntegrationTests.Fixtures.Attributes;
-using Content.Shared.CCVar;
 using Robust.Shared;
 using Robust.Shared.Audio.Components;
 using Robust.Shared.Configuration;
@@ -232,8 +231,6 @@ namespace Content.IntegrationTests.Tests
         /// crude test to try catch issues like this, and possibly should just be disabled.
         /// </remarks>
         [Test]
-        // Prevent gas tanks from exploding during the lifecycle check and leaving temporary explosion entities.
-        [EnsureCVar(Side.Server, typeof(CCVars), nameof(CCVars.AtmosTickRate), 0f)]
         public async Task SpawnAndDeleteEntityCountTest()
         {
             var pair = Pair;
